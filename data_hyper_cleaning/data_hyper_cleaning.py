347725be29d462ad140ba94c23acc605a2bfc321
moriyaya <liuyaohua_918@163.com>
2024-12-14 00:58:18 +0800

    strategy

--- data_hyper_cleaning/data_hyper_cleaning.py
+++ data_hyper_cleaning/data_hyper_cleaning.py
@@ -96,7 +96,7 @@ def main():
     ll_feed_dict = {"data": tr.data.to(device), "target":tr.dirty_target.to(device)}
     for x_itr in range(3):
         if "DM" in boat_config["dynamic_op"] and ("GDA" in boat_config["dynamic_op"]):
-            b_optimizer.boat_configs["DM"]["strategy"] = "s"+str(x_itr+1)
+            b_optimizer._ll_solver.strategy = "s"+str(x_itr+1)
         loss, run_time = b_optimizer.run_iter(ll_feed_dict,ul_feed_dict, current_iter=x_itr)
 
         if x_itr % 1 == 0:
