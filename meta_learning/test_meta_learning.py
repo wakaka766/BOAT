fcdfaa734b56a3539a958b2f7ec37de347600877
moriyaya <liuyaohua_918@163.com>
2024-12-13 16:29:35 +0800

    update_test

--- meta_learning/test_meta_learning.py
+++ meta_learning/test_meta_learning.py
@@ -1,33 +1,87 @@
+# import os
+# import time
+# import shutil
+# t0=time.strftime("%Y_%m_%d_%H_%M_%S")
+# args='meta_learning\method_test'
+# # dynamic_methodlist=(["NGD"],["DI","NGD"],["GDA","NGD","DI"],["DI","NGD","GDA"])
+# # dynamic_method_dm = (["NGD","DM"],["NGD","DM","GDA"])
+# # hyper_methodlist = (["CG"],["CG","PTT"],["RAD"],["RAD","PTT"],["RAD","RGT"],["PTT","RAD","RGT"],["FD"],["FD","PTT"],["NS"],["NS","PTT"],["IGA"])
+# # hyper_method_dm = (["RAD"],["CG"])
+# dynamic_methodlist=(["NGD"],["NGD","GDA"])
+# hyper_methodlist = (["IAD"],["IAD","PTT"],["CG","IAD"],["CG","IAD","PTT"],["NS","IAD"],["NS","IAD","PTT"],["FOA","IAD"],["FOA","IAD","PTT"])
+# # hyper_method_dm = (["RAD"],["CG"])
+# # m='Darts_W_RHG'
+# folder=r'C:\Users\ASUS\Documents\GitHub\BOAT\meta_learning'
+# folder=os.path.join(folder,args,t0)
+# print(folder)
+# if not os.path.exists(folder):
+#     os.makedirs(folder)
+# batfolder=os.path.join(folder,'set.bat')
+# ganfolder=os.path.join(folder,'meta_learning.py')
+# shutil.copyfile('meta_learning.py',ganfolder)
+# with open(batfolder,'w') as f:
+#     k=0
+#     for dynamic_method in dynamic_methodlist:
+#         for hyper_method in hyper_methodlist:
+#             k+=1
+#             print("Comb.{}:".format(k))
+#             print('dynamic_method:',dynamic_method,' hyper_method:',hyper_method )
+#             f.write('python meta_learning.py --dynamic_method {} --hyper_method {} \n'.format(','.join([dynamic for dynamic in dynamic_method]),','.join([hyper for hyper in hyper_method])))
+#
+#
+# os.chdir(folder)
+# os.system(batfolder)
+
+
 import os
 import time
 import shutil
-t0=time.strftime("%Y_%m_%d_%H_%M_%S")
-args='meta_learning\method_test'
-# dynamic_methodlist=(["NGD"],["DI","NGD"],["GDA","NGD","DI"],["DI","NGD","GDA"])
-# dynamic_method_dm = (["NGD","DM"],["NGD","DM","GDA"])
-# hyper_methodlist = (["CG"],["CG","PTT"],["RAD"],["RAD","PTT"],["RAD","RGT"],["PTT","RAD","RGT"],["FD"],["FD","PTT"],["NS"],["NS","PTT"],["IGA"])
-# hyper_method_dm = (["RAD"],["CG"])
-dynamic_methodlist=(["NGD"],["NGD","GDA"])
-hyper_methodlist = (["IAD"],["IAD","PTT"],["CG","IAD"],["CG","IAD","PTT"],["NS","IAD"],["NS","IAD","PTT"],["FOA","IAD"],["FOA","IAD","PTT"])
-# hyper_method_dm = (["RAD"],["CG"])
-# m='Darts_W_RHG'
-folder=r'C:\Users\ASUS\Documents\GitHub\BOAT\meta_learning'
-folder=os.path.join(folder,args,t0)
+import platform
+
+# 获取当前时间
+t0 = time.strftime("%Y_%m_%d_%H_%M_%S")
+args = 'meta_learning/method_test'  # 使用相对路径
+dynamic_methodlist = (["NGD"], ["NGD", "GDA"])
+hyper_methodlist = (
+    ["IAD"], ["IAD", "PTT"], ["CG", "IAD"], ["CG", "IAD", "PTT"], ["NS", "IAD"], ["NS", "IAD", "PTT"], ["FOA", "IAD"], ["FOA", "IAD", "PTT"]
+)
+
+# 获取当前脚本所在的目录（相对路径）
+base_folder = os.path.dirname(os.path.abspath(__file__))  # 获取当前脚本的绝对路径
+folder = os.path.join(base_folder, 'meta_learning', args, t0)  # 构建相对路径
+
 print(folder)
 if not os.path.exists(folder):
     os.makedirs(folder)
-batfolder=os.path.join(folder,'set.bat')
-ganfolder=os.path.join(folder,'meta_learning.py')
-shutil.copyfile('meta_learning.py',ganfolder)
-with open(batfolder,'w') as f:
-    k=0
+
+# 修改 bat 文件为 sh 文件
+script_extension = '.bat' if platform.system() == "Windows" else '.sh'
+script_file = os.path.join(folder, 'set' + script_extension)
+
+# 将 Python 文件复制到目标文件夹
+ganfolder = os.path.join(folder, 'meta_learning.py')
+shutil.copyfile('meta_learning.py', ganfolder)
+
+# 创建批处理或 shell 脚本
+with open(script_file, 'w') as f:
+    k = 0
     for dynamic_method in dynamic_methodlist:
         for hyper_method in hyper_methodlist:
-            k+=1
+            k += 1
             print("Comb.{}:".format(k))
-            print('dynamic_method:',dynamic_method,' hyper_method:',hyper_method )
-            f.write('python meta_learning.py --dynamic_method {} --hyper_method {} \n'.format(','.join([dynamic for dynamic in dynamic_method]),','.join([hyper for hyper in hyper_method])))
+            print('dynamic_method:', dynamic_method, ' hyper_method:', hyper_method)
+            f.write('python meta_learning.py --dynamic_method {} --hyper_method {} \n'.format(
+                ','.join([dynamic for dynamic in dynamic_method]), ','.join([hyper for hyper in hyper_method])))
 
+# 如果是 Ubuntu 系统, 使得脚本具有执行权限
+if platform.system() != "Windows":
+    os.chmod(script_file, 0o775)  # 给 sh 文件执行权限
 
+print('right!')
+
+# 切换到指定文件夹并运行脚本
 os.chdir(folder)
-os.system(batfolder)
+if platform.system() == "Windows":
+    os.system(script_file)  # Windows 下运行 .bat 文件
+else:
+    os.system(f"bash {script_file}")  # Ubuntu 下运行 .sh 文件
