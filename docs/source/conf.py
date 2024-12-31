581fb022c33ba7ed5dc39ad603eecda76844849e
moriyaya <liuyaohua_918@163.com>
2024-12-31 15:59:28 +0800

    rdt

---
+++ docs/source/conf.py
@@ -0,0 +1,42 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'BOAT'
+copyright = '2024, Yaohua Liu'
+author = 'Yaohua Liu'
+release = '0.0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+# Sphinx 配置
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',  # 支持 Google 和 NumPy 风格的 docstring
+    'sphinx.ext.viewcode', # 在文档中生成代码链接
+    'myst_parser',         # 支持 Markdown (可选)
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+
+# html_theme = 'alabaster'
+html_static_path = ['_static']
+
+
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.md': 'markdown',
+}
